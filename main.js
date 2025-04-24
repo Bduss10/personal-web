b1358f32a3da4de57f951c446932f594c47ed189
Abid Abdussalam <Bduss10>
2025-04-24 14:38:27 +0700

    Mulai

---
+++ main.js
@@ -0,0 +1,6 @@
+let nama = 'Abid Abdussalam'
+let usia = 17 
+let tinggiBadan = '178,5'
+let beratBadan = null
+tinggiBadan = 156
+alert(`nama saya ${nama} usia saya ${usia} tahun, tinggi saya ${tinggiBadan}cm, berat badan saya ${beratBadan}`)
